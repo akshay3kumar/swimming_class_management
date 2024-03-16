dc4845cee6e6ebacfbf278ff3c862917fdbe3b48
Akshay Kumar <akshay.g.kumar@capgemini.com>
2024-03-16 21:20:27 +0530

    adding junit to project and added Test class

---
+++ swimmingManagement/src/test/java/SwimmingTest.java
@@ -0,0 +1,14 @@
+
+
+import org.junit.jupiter.api.Test;
+
+public class SwimmingTest {
+
+
+ @Test
+public void registerNewUserTest()
+{
+    SwimmingSystem s= new SwimmingSystem();
+    s.registerNewLearner();
+}
+}
